91750903e3f97cbe3fab7d15b7dcf64b979b1655
dla216 <danny_liu_5@sfu.ca>
2023-03-10 15:07:08 -0800

    add PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float Speed = 10;
+    private Rigidbody2D _rigidbody2D;
+    private float _horizontalMovement;
+    private float _verticalMovement;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _horizontalMovement = Input.GetAxis("Horizontal");
+        _verticalMovement = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody2D.velocity = new Vector2(_horizontalMovement * Speed, _verticalMovement * Speed);
     }
 }
